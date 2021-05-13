71384a88398044d0148056475e9bc9ee4506be2c
Gabriel Yoo <47622991+seunghy1468@users.noreply.github.com>
2021-05-13 18:51:02 +0900

    Delete projects directory
    replace to WebHead_Practice file

--- projects/main.js
+++
@@ -1,8 +0,0 @@
-const toggleButton = document.querySelector('.navbar_toggleButton');
-const menu = document.querySelector('.navbar_menu');
-const sns = document.querySelector('.navbar_sns');
-
-toggleButton.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    sns.classList.toggle('active');
-});
